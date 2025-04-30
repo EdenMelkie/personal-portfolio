5dce18a31739ea6647c8ad792bf1209d98a1a019
Dn Hailemariam Eyayu <hailemariameyayu2012@gmail.com>
2025-04-30 11:36:31 +0300

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
